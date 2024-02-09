e479d755e0a83c7abf9867d30a7077a0bbe89057
Yaami Tachi <nicolas.ferreira@efrei.net>
2024-02-09 10:42:36 +0100

    Ajout footer

--- cra/remote/src/App.js
+++ cra/remote/src/App.js
@@ -1,10 +1,14 @@
+import Footer from '../../sharedComponent/shared/footer';
+import Header from '../../sharedComponent/shared/header';
 import LocalButton from './Button';
 
 const App = () => (
   <div>
+    <Header/>
     <h1>Basic Host-Remote</h1>
     <h2>Remote</h2>
     <LocalButton />
+    <Footer />
   </div>
 );
 
